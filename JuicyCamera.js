a0c0a732094157ba87a21b71a666488bee21cd71
Finlay Curran <finlay.curran@gmail.com>
2014-07-06 19:16:32 +0100

    Added camera lerp

---
+++ JuicyCamera.js
@@ -0,0 +1,28 @@
+JuicyCamera = function (camera, target) {
+
+    this.camera = camera;
+    this.target = target;
+    
+    this.movementSpeed = 0.00000018;
+    
+    this.currentSpeed = 0;
+    
+    this.currentFacingVector = target.position.clone();
+    
+    this.update = function( delta ) {
+    
+        var difference = new THREE.Vector3(0,0,0);
+        
+        difference.subVectors(this.target.position, this.currentFacingVector);
+        difference.multiplyScalar(this.movementSpeed*delta*difference.lengthSq());
+    
+        this.currentFacingVector.add(difference);
+    
+        camera.lookAt(this.currentFacingVector);
+	};
+    
+    this.reset = function() {
+        this.currentFacingVector = target.position.clone();
+        camera.lookAt(this.currentFacingVector);
+    }
+}
